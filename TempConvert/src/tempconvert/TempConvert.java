99f73db0815f63f19d3b76f1a6150f797ddbf665
N <N@BNA-OB>
2018-09-29 22:13:16 -0400

    9/25/18 - created project for first assignment

--- TempConvert/src/tempconvert/TempConvert.java
+++ TempConvert/src/tempconvert/TempConvert.java
@@ -1,25 +1,25 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Program created for CS 260. Program converts temperature in 
+ * degreees Fahrenheit to Celcius. 
  */
-//package tempconvert;
 
 /**
  *
- * @author N
+ * @author Nicholas Holmes
  */
 import java.util.Scanner;
 
-public class TempConvert {
+public class TempConvert 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         // TODO code application logic here
         float n1, temp;
-        Scanner keyboard = new Scanner (System.in);
+        Scanner keyboard = new Scanner(System.in);
         
         System.out.println("Enter the temperature in degrees Celsius ");
         n1 = keyboard.nextFloat();
